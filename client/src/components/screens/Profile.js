c75805402bcfe70c8e568d8dce7bee32949a251e
sharmishtha2000 <sharmishtha2000@gmail.com>
2021-07-05 13:20:43 +0530

    Final changes

---
+++ client/src/components/screens/Profile.js
@@ -0,0 +1,116 @@
+import React,{useEffect,useState,useContext} from 'react'
+import {UserContext} from '../../App'
+
+const Profile  = ()=>{
+    const [mypics,setPics] = useState([])
+    const {state,dispatch} = useContext(UserContext)
+    const [image,setImage] = useState("")
+    useEffect(()=>{
+       fetch('/mypost',{
+           headers:{
+               "Authorization":"Bearer "+localStorage.getItem("jwt")
+           }
+       }).then(res=>res.json())
+       .then(result=>{
+           console.log(result)
+           setPics(result.mypost)
+       })
+    },[])
+    useEffect(()=>{
+       if(image){
+        const data = new FormData()
+        data.append("file",image)
+        data.append("upload_preset","insta-clone")
+        data.append("cloud_name","sam2000")
+        fetch("https://api.cloudinary.com/v1_1/sam2000/image/upload",{
+            method:"post",
+            body:data
+        })
+        .then(res=>res.json())
+        .then(data=>{
+    
+       
+           fetch('/updatepic',{
+               method:"put",
+               headers:{
+                   "Content-Type":"application/json",
+                   "Authorization":"Bearer "+localStorage.getItem("jwt")
+               },
+               body:JSON.stringify({
+                   pic:data.url
+               })
+           }).then(res=>res.json())
+           .then(result=>{
+               console.log(result)
+               localStorage.setItem("user",JSON.stringify({...state,pic:result.pic}))
+               dispatch({type:"UPDATEPIC",payload:result.pic})
+               //window.location.reload()
+           })
+       
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+       }
+    },[image])
+    const updatePhoto = (file)=>{
+        setImage(file)
+    }
+   return (
+       <div style={{maxWidth:"550px",margin:"0px auto"}}>
+           <div style={{
+              margin:"18px 0px",
+               borderBottom:"1px solid grey"
+           }}>
+
+         
+           <div style={{
+               display:"flex",
+               justifyContent:"space-around",
+              
+           }}>
+               <div>
+                   <img style={{width:"160px",height:"160px",borderRadius:"50%"}}
+                   src={state?state.pic:"loading"}
+                   />
+                 
+               </div>
+               <div>
+                   <h4 style={{fontWeight: "bold"}}>{state?state.name:"loading"}</h4>
+                   <h6>{state?state.email:"loading"}</h6>
+                   <div style={{display:"flex",justifyContent:"space-between",width:"108%"}}>
+                       <h6>{mypics.length} posts</h6>
+                       <h6>{state?state.followers.length:"0"} followers</h6>
+                       <h6>{state?state.following.length:"0"} following</h6>
+                   </div>
+
+               </div>
+           </div>
+        
+            <div className="file-field input-field" style={{margin:"10px"}}>
+            <div className="logout-btn btn #424242 grey darken-3">
+                <span>Update pic</span>
+                <input type="file" onChange={(e)=>updatePhoto(e.target.files[0])} />
+            </div>
+            <div className="file-path-wrapper">
+                <input className="file-path validate" type="text" />
+            </div>
+            </div>
+            </div>      
+           <div className="gallery">
+               {
+                   mypics.map(item=>{
+                       return(
+                        <img key={item._id} className="item" src={item.photo} alt={item.title} style={{borderRadius: "10px"}} />  
+                       )
+                   })
+               }
+
+           
+           </div>
+       </div>
+   )
+}
+
+
+export default Profile
---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports={
+    MONGOURI:"mongodb+srv://sam:o6Z9l7p8Tw0erHwM@cluster0.ngs25.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET:"hdggfisdugijsdbcccj",
+    SENDGRID_API:process.env.SENDGRID_API,
+    EMAIL:process.env.EMAIL
+}
